37c3f5969c6f84d10dc92a777308926ae3cacfaf
Branko Vukelic <branko@brankovukelic.com>
2013-11-04 09:32:45 +0100

    Changed the API completely to allow chaining

--- test/testmy.js
+++ test/testmy.js
@@ -4,81 +4,77 @@
   var assert = test.assert;
 
   test('select element by id', function () {
-    var node = my.id('h1');
+    var node = my().id('h1');
     assert.element(node);
   });
 
   test('select elements by class', function () {
-    var nodes = my.cls('li');
+    var nodes = my().cls('li');
     assert.elements(nodes);
   });
 
   test('selecting .li returns 12 elements', function () {
-    var nodes = my.cls('li');
+    var nodes = my().cls('li');
     assert.count(nodes, 12);
   });
 
   test('select elemetns by query', function () {
-    var node = my.get('#ul1 #liA6');
+    var node = my().get('#ul1 #liA6');
     assert.element(node);
   });
 
   test('select multiple elements by query', function () {
-    var nodes = my.find('#ul1 .li');
+    var nodes = my().find('#ul1 .li');
     assert.elements(nodes);
   });
 
   test('selecting all .li inside #ul1 returns 6 elements', function () {
-    var nodes = my.find('#ul1 .li');
+    var nodes = my().find('#ul1 .li');
     assert.count(nodes, 6);
   });
 
   test('attach event listener', function(done) {
-    var anchor = my.id('anc');
+    var anchor = my().id('anc');
     var listener = function(e) {
       e.preventDefault();
       done(test.noop);
       this.removeEventListener('click', listener);
     };
-    my.on(anchor, 'click', listener);
+    my(anchor).on('click', listener);
     test.note('click the \'anchor\' link');
   });
 
   test('attaching event listener returns origina object', function() {
-    var anchor = my.id('anc');
-    var ret = my.on(anchor, 'click', test.noop);
-    assert.element(ret);
-    assert.equal(ret, anchor);
+    var anchor = my().id('anc');
+    var m = my(anchor);
+    var ret = m.on('click', test.noop);
+    assert.equal(ret, m);
   });
 
   test('firing native event', function(done) {
-    var anchor = my.id('anc');
+    var anchor = my().id('anc');
     var listener = function(e) {
       e.preventDefault();
       done(test.noop);
       this.removeEventListener('focus', listener);
     };
-    my.on(anchor, 'focus', listener);
-    my.fire(anchor, 'focus');
-  });
-
-  test('firing native event returns event object', function() {
-    var anchor = my.id('anc');
-    var ret = my.fire(anchor, 'focus');
-    assert.ctor(ret, window.FocusEvent);
+    my(anchor).on('focus', listener).fire('focus');
   });
 
   test('unbind event', function(done) {
-    var el = my.id('liA1');
+    var el = my().id('liA1');
+    var m = my(el);
     var called = false;
     var listener = function(e) {
       e.preventDefault();
       called = true;
       this.removeEventListener('mousein', listener, true);
     };
-    my.on(el, 'mousein', listener, true);
-    my.off(el, 'mousein', listener, true);
-    my.fire(el, 'mousein');
+
+    m.on('mousein', listener, true).
+      off('mousein', listener, true).
+      fire('mousein');
+
     test.later(function() {
       done(function() {
         assert.false(called);
@@ -87,7 +83,8 @@
   });
 
   test('unbind all events of same type', function(done) {
-    var el = my.id('liA1');
+    var el = my().id('liA1');
+    var m = my(el);
     var called1 = false;
     var called2 = false;
     var called3 = false;
@@ -106,12 +103,14 @@
       called3 = true;
       this.removeEventListener('mouseout', listener3);
     };
-    my.on(el, 'mousein', listener1);
-    my.on(el, 'mousein', listener2);
-    my.on(el, 'mouseout', listener3);
-    my.off(el, 'mousein');
-    my.fire(el, 'mousein');
-    my.fire(el, 'mouseout');
+
+    m.on('mousein', listener1).
+      on('mousein', listener2).
+      on('mouseout', listener3).
+      off('mousein').
+      fire('mousein').
+      fire('mouseout');
+
     test.later(function() {
       done(function() {
         assert.false(called1);
@@ -122,7 +121,8 @@
   });
 
   test('unbind only handlers using some func', function(done) {
-    var el = my.id('liA1');
+    var el = my().id('liA1');
+    var m = my(el);
     var called1 = false;
     var called2 = false;
     var called3 = false;
@@ -141,12 +141,14 @@
       called3 = true;
       this.removeEventListener('mouseout', listener3);
     };
-    my.on(el, 'mousein', listener1, false);
-    my.on(el, 'mousein', listener2, true);
-    my.on(el, 'mouseout', listener3, false);
-    my.off(el, listener2);
-    my.fire(el, 'mousein');
-    my.fire(el, 'mouseout');
+
+    m.on('mousein', listener1, false).
+      on('mousein', listener2, true).
+      on('mouseout', listener3, false).
+      off(listener2).
+      fire('mousein').
+      fire('mouseout');
+
     test.later(function() {
       done(function() {
         assert.true(called1);
@@ -157,7 +159,8 @@
   });
 
   test('unbind only handlers that used capture', function(done) {
-    var el = my.id('liA1');
+    var el = my().id('liA1');
+    var m = my(el);
     var called1 = false;
     var called2 = false;
     var called3 = false;
@@ -176,12 +179,14 @@
       called3 = true;
       this.removeEventListener('mouseout', listener3);
     };
-    my.on(el, 'mousein', listener1, false);
-    my.on(el, 'mousein', listener2, true);
-    my.on(el, 'mouseout', listener3, false);
-    my.off(el, true);
-    my.fire(el, 'mousein');
-    my.fire(el, 'mouseout');
+
+    m.on('mousein', listener1, false).
+      on('mousein', listener2, true).
+      on('mouseout', listener3, false).
+      off(true).
+      fire('mousein').
+      fire('mouseout');
+
     test.later(function() {
       done(function() {
         assert.true(called1);
@@ -192,7 +197,8 @@
   });
 
   test('unbind handlers of type that did not use capture', function(done) {
-    var el = my.id('liA1');
+    var el = my().id('liA1');
+    var m = my(el);
     var called1 = false;
     var called2 = false;
     var called3 = false;
@@ -211,12 +217,14 @@
       called3 = true;
       this.removeEventListener('mouseout', listener3);
     };
-    my.on(el, 'mousein', listener1, false);
-    my.on(el, 'mousein', listener2, true);
-    my.on(el, 'mouseout', listener3, false);
-    my.off(el, 'mousein', false);
-    my.fire(el, 'mousein');
-    my.fire(el, 'mouseout');
+
+    m.on('mousein', listener1, false).
+      on('mousein', listener2, true).
+      on('mouseout', listener3, false).
+      off('mousein', false).
+      fire('mousein').
+      fire('mouseout');
+
     test.later(function() {
       done(function() {
         assert.false(called1);
@@ -227,52 +235,45 @@
   });
 
   test('get class names', function() {
-    var el = my.id('test2');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor']);
+    var m = my(my().id('test2'));
+    assert.array(m.class(), ['test', 'anchor']);
   });
 
   test('set class name', function() {
-    var el = my.id('test2');
-    my.class(el, 'foo');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor', 'foo']);
+    var m = my(my().id('test2'));
+    m.class('foo');
+    assert.array(m.class(), ['test', 'anchor', 'foo']);
   });
 
   test('does not add existing classes', function() {
-    var el = my.id('test2');
-    my.class(el, 'test');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor', 'foo']);
+    var m = my(my().id('test2'));
+    m.class('test');
+    assert.array(m.class(), ['test', 'anchor', 'foo']);
   });
 
   test('set multiple classes', function() {
-    var el = my.id('test2');
-    my.class(el, 'bar', 'baz');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor', 'foo', 'bar', 'baz']);
+    var m = my(my().id('test2'));
+    m.class('bar', 'baz');
+    assert.array(m.class(), ['test', 'anchor', 'foo', 'bar', 'baz']);
   });
 
   test('remove class', function() {
-    var el = my.id('test2');
-    my.removeClass(el, 'baz');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor', 'foo', 'bar']);
+    var m = my(my().id('test2'));
+    m.removeClass('baz');
+    assert.array(m.class(), ['test', 'anchor', 'foo', 'bar']);
   });
 
   test('remove multiple classes', function() {
-    var el = my.id('test2');
-    my.removeClass(el, 'bar', 'foo');
-    var classes = my.class(el);
-    assert.array(classes, ['test', 'anchor']);
+    var m = my(my().id('test2'));
+    m.removeClass('bar', 'foo');
+    assert.array(m.class(), ['test', 'anchor']);
   });
 
   test('remove all classes', function() {
-    var el = my.id('test2');
-    my.removeClass(el);
-    var classes = my.class(el);
-    assert.count(classes, 0);
-    my.class(el, 'test', 'anchor'); // restore original classes
+    var m = my(my().id('test2'));
+    m.removeClass();
+    assert.count(m.class(), 0);
+    m.class('test', 'anchor'); // restore original classes
   });
 
 }(this.my, this.test));
